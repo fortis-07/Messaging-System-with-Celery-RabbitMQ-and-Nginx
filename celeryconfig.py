6d72051a66e755e1e41537d40ad07a7e9b87e403
fortis-07 <fortismanuel@gmail.com>
2024-07-13 21:13:03 +0100

    Delete celeryconfig.py

--- celeryconfig.py
+++
@@ -1,7 +0,0 @@
-# The broker URL for Celery is responsible for sending and receiving messages between Celery workers and the client
-# Here, the broker URL is configured to use RabbitMQ (with default guest user) running on localhost
-broker_url = 'pyamqp://guest@localhost//'
-
-# The result backend is used to store the results of Celery tasks
-# Here, the result backend is configured to use RPC (Remote Procedure Call)
-result_backend = 'rpc://'
